963027dad91e79896a6e20654572f26f2b0b6501
Miyazaki Masatoshi <ma.miyazaki@soraniwa.co.jp>
2021-06-07 14:03:29 +0900

    refactor: remove unnecessary proto

--- client/main.go
+++ client/main.go
@@ -6,34 +6,11 @@ import (
 
 	"github.com/pkg/errors"
 
-	pb "github.com/ma-miyazaki/go-grpc-neo4j-example/pb/calc"
 	"github.com/ma-miyazaki/go-grpc-neo4j-example/pb/employee"
 	"github.com/rs/zerolog/log"
 
 	"google.golang.org/grpc"
 )
-
-func requestSum(ctx context.Context, conn *grpc.ClientConn, a, b int32) error {
-	client := pb.NewCalcClient(conn)
-	sumRequest := pb.SumRequest{
-		A: a,
-		B: b,
-	}
-	reply, err := client.Sum(ctx, &sumRequest)
-	if err != nil {
-		return errors.Wrap(err, "受取り失敗")
-	}
-	log.Info().Msgf("Reply calc. [%v]", reply.GetMessage())
-	return nil
-}
-
-func sum(a, b int32) error {
-	return doWithConnection(func(conn *grpc.ClientConn) error {
-		return doInTimeout(func(ctx context.Context) error {
-			return requestSum(ctx, conn, a, b)
-		})
-	})
-}
 
 func requestAddEmployee(ctx context.Context, conn *grpc.ClientConn, email, lastName, firstName string) error {
 	client := employee.NewEmployeeServiceClient(conn)
@@ -81,11 +58,6 @@ func doWithConnection(fx func(*grpc.ClientConn) error) error {
 }
 
 func main() {
-	// a := int32(300)
-	// b := int32(500)
-	// if err := sum(a, b); err != nil {
-	// 	log.Fatalf("%v", err)
-	// }
 	if err := addEmployee("test@example.com", "高嶺", "朋樹"); err != nil {
 		log.Fatal().Stack().Err(err).Msg("gRPC request error")
 	}
--- server/main.go
+++ server/main.go
@@ -7,27 +7,12 @@ import (
 	"github.com/ma-miyazaki/go-grpc-neo4j-example/server/infrastracture/persistence"
 	"github.com/ma-miyazaki/go-grpc-neo4j-example/server/interface/handler"
 	"github.com/ma-miyazaki/go-grpc-neo4j-example/server/usecase"
-	// "github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 
 	"google.golang.org/grpc"
 )
 
 const port = ":50051"
-
-// type ServerUnary struct {
-// 	pb.UnimplementedCalcServer
-// }
-
-// func (s *ServerUnary) Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumReply, error) {
-// 	a := in.GetA()
-// 	b := in.GetB()
-// 	fmt.Println(a, b)
-// 	reply := fmt.Sprintf("%d + %d = %d", a, b, a+b)
-// 	return &pb.SumReply{
-// 		Message: reply,
-// 	}, nil
-// }
 
 func createEmployeeServer() employee.EmployeeServiceServer {
 	employeeRepository := persistence.NewEmployeeRepository()
@@ -36,16 +21,16 @@ func createEmployeeServer() employee.EmployeeServiceServer {
 }
 
 func main() {
+	defer persistence.CloseNeo4jDriver()
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatal().Stack().Err(err).Msg("failed to listen")
 		return
 	}
 	s := grpc.NewServer()
-	// pb.RegisterCalcServer(s, &ServerUnary{})
 	employee.RegisterEmployeeServiceServer(s, createEmployeeServer())
 
-	defer persistence.CloseNeo4jDriver()
 	if err := s.Serve(lis); err != nil {
 		log.Fatal().Stack().Err(err).Msg("failed to serve")
 		return
